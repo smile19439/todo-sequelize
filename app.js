53024d2ad1c9c95f405b0d1d37d115ce78736b29
smile19439 <smile19439@gmail.com>
2022-05-09 23:29:02 +0800

    add sequelize

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 const app = express()
 const PORT = 3000
 
@@ -13,6 +17,29 @@ app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  // res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(PORT, () => {
