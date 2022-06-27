b02b5ba160af58df27c892b28e213c503901ab5f
smile19439 <smile19439@gmail.com>
2022-06-27 18:38:30 +0800

    add user login, register, main page and details

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const passport = require('passport')
+
+const usePassport = require('./config/passport')
 
 const db = require('./models')
 const Todo = db.Todo
@@ -12,30 +16,65 @@ const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(express.urlencoded({ extended : true }))
+
+app.use(session({
+  secret: 'TodoSecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+usePassport(app)
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then(todos => res.render('index', { todos }))
+    .catch(error => res.status(422).json(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
 })
 
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
 })
 
 app.post('/users/register', (req, res) => {
-  // res.send('register')
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        console.log('user already exits.')
+        return res.render('register', { name, email, password, confirmPassword })
+      }
+      return bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+    })
 })
 
 app.get('/users/logout', (req, res) => {
