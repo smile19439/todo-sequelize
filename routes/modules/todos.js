5f716ec7e77e50181e9aa05c1d31d54245782a47
smile19439 <smile19439@gmail.com>
2022-06-28 19:10:15 +0800

    add delete function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -45,4 +45,11 @@ router.put('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.destroy({ where: { id } })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
